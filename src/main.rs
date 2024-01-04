01d5a4f94c325d137654f205b9e45f7e877145a6
Anders Njøs Slinde <anders.slinde@gmail.com>
2024-01-04 22:09:47 +0100

    Stage 4

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,44 @@
-use std::process::Stdio;
-
 use anyhow::{Context, Result};
+use std::{os::unix::fs, path::Path, process::Stdio};
 
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 fn main() -> Result<()> {
-    // Uncomment this block to pass the first stage!
     let args: Vec<_> = std::env::args().collect();
     let command = &args[3];
     let command_args = &args[4..];
-    let output = std::process::Command::new(command)
+
+    // Create temp dir
+    let temp_dir = tempfile::tempdir()?;
+    let temp_dir_path = temp_dir.into_path();
+
+    // Because of some weirdness with chroot, we need to create dev/null
+    std::fs::create_dir_all(temp_dir_path.join("dev"))?;
+    std::fs::File::create(temp_dir_path.join("dev/null"))?;
+
+    let command_path = Path::new(command)
+        .file_name()
+        .context("Command is an invalid filename?")?;
+    std::fs::copy(Path::new(command), temp_dir_path.join(command_path))
+        .context("Could not copy command to temp directory")?;
+
+    fs::chroot(temp_dir_path.as_path())?;
+    std::env::set_current_dir("/")?;
+    let command_call = Path::new("/").join(command_path);
+
+    let output = std::process::Command::new(command_call)
+        .current_dir("/")
         .args(command_args)
-        .stdout(Stdio::piped())
-        .stderr(Stdio::piped())
+        .stdin(Stdio::inherit())
+        .stdout(Stdio::inherit())
+        .stderr(Stdio::inherit())
+        .env_clear()
         .output()
         .with_context(|| {
             format!(
-                "Tried to run '{}' with arguments {:?}",
-                command, command_args
+                "Tried to run '{}' with arguments {:?}, in temp file {:?}",
+                command_path.to_str().unwrap(),
+                command_args,
+                temp_dir_path
             )
         })?;
 
